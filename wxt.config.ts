a2423cb49c8ff8e1cf4c0c3d2d8a352caaa301f9
Shohei Jinno <shohei.jinno@gmail.com>
2024-12-26 20:04:43 -0800

    chore: move source code to src

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-react'],
+  extensionApi: "chrome",
+  modules: ["@wxt-dev/module-react"],
+  srcDir: "src", // default: "."
+  outDir: "dist", // default: ".output"
 });
